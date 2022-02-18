4a14284f0a562a0f75fd47c8607c639223b5049f
gsdr3112 <gsdr3112@hotmail.com>
2022-02-18 17:53:54 -0300

    Operadores relacionais

--- src/one/digitalinnovation/basecamp/Main.java
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -45,7 +45,7 @@ public class Main {
 
 
      }*/
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String s1 = "C";
         String s2 = "C";
         char c1 = 'x';
@@ -73,6 +73,119 @@ public class Main {
         boolean recebeAuxilio = salarioBaixo && muitosDependentes;
 
         System.out.println("Recebe auxílio: " + recebeAuxilio);
+
+    }*/
+
+    public static void main(String[] args) {
+        int mes = 12;
+
+        switch (mes) {
+            case 1:
+                System.out.println("janeiro");
+                break;
+            case 2:
+                System.out.println("fevereiro");
+                break;
+            case 3:
+                System.out.println("março");
+                break;
+            case 4:
+                System.out.println("abril");
+                break;
+            case 5:
+                System.out.println("maio");
+                break;
+            case 6:
+                System.out.println("junho");
+                break;
+            case 7:
+                System.out.println("julho");
+                break;
+            case 8:
+                System.out.println("agosto");
+                break;
+            case 9:
+                System.out.println("setembro");
+                break;
+            case 10:
+                System.out.println("outubro");
+                break;
+            case 11:
+                System.out.println("novembro");
+                break;
+            case 12:
+                System.out.println("dezembro");
+                break;
+            default:
+                System.out.println("digite um número de mês válido");
+                break;
+
+        }
+        System.out.println();
+
+        if (mes == 1) {
+            System.out.println("janeiro");
+        } else if (mes == 2) {
+            System.out.println("fevereiro");
+        } else if (mes == 3) {
+            System.out.println("março");
+        } else if (mes == 4) {
+            System.out.println("abril");
+        } else if (mes == 5) {
+            System.out.println("maio");
+        } else if (mes == 6) {
+            System.out.println("junho");
+        } else if (mes == 7) {
+            System.out.println("julho");
+        } else if (mes == 8) {
+            System.out.println("agosto");
+        } else if (mes == 9) {
+            System.out.println("setembro");
+        } else if (mes == 10) {
+            System.out.println("outubro");
+        } else if (mes == 11) {
+            System.out.println("novembro");
+        } else if (mes == 12) {
+            System.out.println("dezembro");
+        } else {
+            System.out.println("digite um número de mês válido");
+        }
+
+
+        if ((mes == 7) || (mes == 12) || (mes == 1)) {
+            System.out.println("Férias");
+        }
+
+        System.out.println();
+
+        String diaSemana = "sabado";
+
+        switch (diaSemana){
+            case "segunda":
+                System.out.println(1);
+                break;
+            case "terça":
+                System.out.println(2);
+                break;
+            case "quarta":
+                System.out.println(3);
+                break;
+            case "quinta":
+                System.out.println(4);
+                break;
+            case "sexta":
+                System.out.println(5);
+                break;
+            case "sabado":
+                System.out.println(6);
+                break;
+            case "domingo":
+                System.out.println(7);
+                break;
+            default:
+                System.out.println("Valor indefinido");
+        }
+
 
     }
 
